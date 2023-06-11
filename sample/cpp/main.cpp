e3a5a1dc094ccb8b3acb678fb05cf90fd2b22384
nekosu <liao.hy@outlook.com>
2023-06-11 22:47:33 +0800

    feat: 优化cmakelist中的包含逻辑
    允许sample配置adb目标(虽然这玩意应该不是用来测试的

--- sample/cpp/main.cpp
+++ sample/cpp/main.cpp
@@ -9,8 +9,23 @@
 bool demo_polling(const std::filesystem::path& cur_dir);
 bool demo_waiting(const std::filesystem::path& cur_dir);
 
+#ifdef _WIN32
+const char* adb = "adb.exe";
+#else
+const char* adb = "adb";
+#endif
+
+std::string adbAddress;
+
 int main([[maybe_unused]] int argc, char** argv)
 {
+    if (argc > 1) {
+        adbAddress = argv[1];
+    }
+    else {
+        adbAddress = "127.0.0.1:5555";
+    }
+
     const auto cur_dir = std::filesystem::path(argv[0]).parent_path();
 
     MaaSetGlobalOption(MaaGlobalOption_Logging, (cur_dir / "debug").string().c_str());
@@ -26,7 +41,7 @@ bool demo_polling(const std::filesystem::path& cur_dir)
     auto resource_handle = MaaResourceCreate((cur_dir / "cache").string().c_str(), nullptr, nullptr);
     auto resource_id = MaaResourcePostResource(resource_handle, (cur_dir / "resource").string().c_str());
 
-    auto controller_handle = MaaAdbControllerCreate("adb.exe", "127.0.0.1:5555", nullptr, nullptr);
+    auto controller_handle = MaaAdbControllerCreate(adb, adbAddress.c_str(), nullptr, nullptr);
     auto connection_id = MaaControllerPostConnection(controller_handle);
 
     for (auto status = MaaResourceStatus(resource_handle, resource_id);
@@ -70,7 +85,7 @@ bool demo_waiting(const std::filesystem::path& cur_dir)
 {
     auto maa_handle = MaaCreate(nullptr, nullptr);
     auto resource_handle = MaaResourceCreate((cur_dir / "cache").string().c_str(), nullptr, nullptr);
-    auto controller_handle = MaaAdbControllerCreate("adb.exe", "127.0.0.1:5555", nullptr, nullptr);
+    auto controller_handle = MaaAdbControllerCreate(adb, adbAddress.c_str(), nullptr, nullptr);
 
     MaaBindResource(maa_handle, resource_handle);
     MaaBindController(maa_handle, controller_handle);
