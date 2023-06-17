1431bfa4abaf7ff2a1ffeae8a81c115d003fd5c4
nekosu <liao.hy@outlook.com>
2023-06-18 00:53:38 +0800

    fix: 修复sdkList顺序

--- source/Controller/Unit/Screencap/Minicap/MinicapBase.cpp
+++ source/Controller/Unit/Screencap/Minicap/MinicapBase.cpp
@@ -18,7 +18,7 @@ bool MinicapBase::parse(const json::value& config)
 // arm64-v8会卡住, 不知道原因
 static constexpr std::array<std::string_view, 3> kArchList = { /* "x86_64", */ "x86", /* "arm64-v8a", */ "armeabi-v7a",
                                                                "armeabi" };
-static constexpr std::array<int, 16> kSdkList = { 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31 };
+static constexpr std::array<int, 16> kSdkList = { 31, 29, 28, 27, 26, 25, 24, 23, 22, 21, 19, 18, 17, 16, 15, 14 };
 
 bool MinicapBase::init(int w, int h, std::function<std::string(const std::string&)> path_of_bin,
                        std::function<std::string(const std::string&, int)> path_of_lib, const std::string& force_temp)
