5206a8fb0ce528ce3adaeab5116a95ecdb7b5674
nekosu <liao.hy@outlook.com>
2023-06-20 18:21:17 +0800

    feat: maamsg注释

--- include/MaaMsg.h
+++ include/MaaMsg.h
@@ -2,13 +2,28 @@
 
 #define MAAMSG_INVALID ("Invalid")
 
+/*
+    {
+        id: number,
+        path: [string, resource path]
+    }
+*/
 #define MAAMSG_RESOURCE_START_LOADING ("Resource.StartLoading")
 #define MAAMSG_RESOURCE_LOADING_COMPLETED ("Resource.LoadingCompleted")
 #define MAAMSG_RESOURCE_LOADING_ERROR ("Resource.LoadingError")
 
+/* {} */
 #define MAAMSG_CONTROLLER_CONNECTED ("Controller.Connected")
 #define MAAMSG_CONTROLLER_CONNECT_FAILED ("Controller.ConnectFailed")
 
+/*
+    {
+        id: number,
+        type: [string, task type],
+        uuid: [string, controller id],
+        hash: [string, resource hash]
+    }
+*/
 #define MAAMSG_TASK_STARTED ("Task.Started")
 #define MAAMSG_TASK_COMPLETED ("Task.Completed")
 #define MAAMSG_TASK_FAILED ("Task.Failed")
