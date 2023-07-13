24817b5327e241b4918783c49b594e5d6baa7328
MistEO <mistereo@hotmail.com>
2023-07-13 23:58:08 +0800

    chore: 移动文件夹

--- source/API/MaaAPI.cpp
+++ source/API/MaaAPI.cpp
@@ -231,15 +231,12 @@ MaaSize MaaControllerGetImage(MaaControllerHandle ctrl, void* buff, MaaSize buff
 {
     LogFunc << VAR_VOIDP(ctrl) << VAR_VOIDP(buff) << VAR(buff_size);
 
-    if (!ctrl) {
+    if (!ctrl || !buff) {
         return MaaNullSize;
     }
     auto image = ctrl->get_image_cache();
     size_t size = image.size();
-    if (!buff) {
-        return size;
-    }
-    if (size > buff_size) {
+    if (size >= buff_size) {
         return MaaNullSize;
     }
     memcpy(buff, image.data(), size);
