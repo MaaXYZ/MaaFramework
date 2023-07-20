18f025d653f1364c173eb33aed6d78c2c11a6cb1
nekosu <liao.hy@outlook.com>
2023-07-21 01:23:38 +0800

    fix: onnx searching and linking

--- cmake/modules/FindONNXRuntime.cmake
+++ cmake/modules/FindONNXRuntime.cmake
@@ -1,17 +1,31 @@
 find_path(ONNXRuntime_INCLUDE_DIR NAMES onnxruntime/core/session/onnxruntime_c_api.h)
 
-find_library(ONNXRuntime_LIBRARY NAMES onnxruntime)
+find_library(ONNXRuntime_LIBRARY_IMP NAMES onnxruntime)
+
+if (WIN32)
+    get_filename_component(ONNXRuntime_PATH_LIB ${ONNXRuntime_LIBRARY_IMP} DIRECTORY)
+    find_file(ONNXRuntime_LIBRARY NAMES onnxruntime_maa.dll PATHS "${ONNXRuntime_PATH_LIB}/../bin")
+else ()
+    set(ONNXRuntime_LIBRARY ${ONNXRuntime_LIBRARY_IMP})
+endif (WIN32)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     ONNXRuntime
-    REQUIRED_VARS ONNXRuntime_LIBRARY ONNXRuntime_INCLUDE_DIR
+    REQUIRED_VARS ONNXRuntime_LIBRARY_IMP ONNXRuntime_INCLUDE_DIR
 )
 
 if(ONNXRuntime_FOUND)
     set(ONNXRuntime_INCLUDE_DIRS ${ONNXRuntime_INCLUDE_DIR})
     if(NOT TARGET ONNXRuntime::ONNXRuntime)
-        add_library(ONNXRuntime::ONNXRuntime UNKNOWN IMPORTED)
+        add_library(ONNXRuntime::ONNXRuntime SHARED IMPORTED)
+        if (WIN32)
+            set_target_properties(ONNXRuntime::ONNXRuntime PROPERTIES
+                IMPORTED_IMPLIB_DEBUG "${ONNXRuntime_LIBRARY_IMP}"
+                IMPORTED_IMPLIB_RELEASE "${ONNXRuntime_LIBRARY_IMP}"
+                IMPORTED_IMPLIB_RELWITHDEBINFO "${ONNXRuntime_LIBRARY_IMP}"
+            )
+        endif (WIN32)
         set_target_properties(ONNXRuntime::ONNXRuntime PROPERTIES
             IMPORTED_CONFIGURATIONS "Debug;Release;RelWithDebInfo"
             IMPORTED_LOCATION_DEBUG "${ONNXRuntime_LIBRARY}"
