ec1c00d2aca563a701cccfc114613fdfd0d43548
MistEO <mistereo@hotmail.com>
2023-08-02 18:06:30 +0800

    feat: toolkit 更多的大饼

--- include/MaaToolKit/MaaToolKitAPI.h
+++ include/MaaToolKit/MaaToolKitAPI.h
@@ -14,42 +14,58 @@ extern "C"
     MaaToolKitConfigHandle MAA_TOOLKIT_API MaaToolKitGetCurrentConfig();
     MaaBool MAA_TOOLKIT_API MaaToolKitSetCurrentConfig(MaaToolKitConfigHandle config_handle);
 
-    MaaToolKitConfigHandle MAA_TOOLKIT_API MaaToolKitAddConfig(MaaString config_name, MaaString copy_from);
+    MaaToolKitConfigHandle MAA_TOOLKIT_API MaaToolKitAddConfig(MaaString config_name, MaaToolKitConfigHandle copy_from);
     MaaBool MAA_TOOLKIT_API MaaToolKitDelConfig(MaaString config_name);
 
     MaaString MAA_TOOLKIT_API MaaToolKitGetConfigName(MaaToolKitConfigHandle config_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetConfigName(MaaToolKitConfigHandle config_handle, MaaString new_name);
+    MaaString MAA_TOOLKIT_API MaaToolKitGetConfigDescription(MaaToolKitConfigHandle config_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitSetConfigDescription(MaaToolKitConfigHandle config_handle,
+                                                           MaaString new_description);
+
+    MaaSize MAA_TOOLKIT_API MaaToolKitFindDevice();
+    MaaString MAA_TOOLKIT_API MaaToolKitGetDeviceName(MaaSize index);
+    MaaString MAA_TOOLKIT_API MaaToolKitGetDeviceAdbPath(MaaSize index);
+    MaaString MAA_TOOLKIT_API MaaToolKitGetDeviceAdbSerial(MaaSize index);
+    MaaJsonString MAA_TOOLKIT_API MaaToolKitGetDeviceAdbConfig(MaaSize index);
+
     MaaString MAA_TOOLKIT_API MaaToolKitGetAdbPath(MaaToolKitConfigHandle config_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetAdbPath(MaaToolKitConfigHandle config_handle, MaaString new_path);
     MaaString MAA_TOOLKIT_API MaaToolKitGetAdbSerial(MaaToolKitConfigHandle config_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetAdbSerial(MaaToolKitConfigHandle config_handle, MaaString new_serial);
+    MaaJsonString MAA_TOOLKIT_API MaaToolKitGetAdbConfig(MaaToolKitConfigHandle config_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitSetAdbConfig(MaaToolKitConfigHandle config_handle, MaaJsonString new_config);
 
     MaaSize MAA_TOOLKIT_API MaaToolKitTaskSize(MaaToolKitConfigHandle config_handle);
     MaaToolKitTaskHandle MAA_TOOLKIT_API MaaToolKitGetTask(MaaToolKitConfigHandle config_handle, MaaSize index);
 
     MaaToolKitTaskHandle MAA_TOOLKIT_API MaaToolKitAddTask(MaaToolKitConfigHandle config_handle, MaaString task_name,
-                                                           MaaString copy_from);
+                                                           MaaToolKitTaskHandle copy_from);
     MaaBool MAA_TOOLKIT_API MaaToolKitDelTask(MaaToolKitConfigHandle config_handle, MaaString task_name);
 
     MaaString MAA_TOOLKIT_API MaaToolKitGetTaskName(MaaToolKitTaskHandle task_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskName(MaaToolKitTaskHandle task_handle, MaaString new_name);
+    MaaString MAA_TOOLKIT_API MaaToolKitGetTaskDescription(MaaToolKitConfigHandle config_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskDescription(MaaToolKitConfigHandle config_handle,
+                                                           MaaString new_description);
     MaaString MAA_TOOLKIT_API MaaToolKitGetTaskType(MaaToolKitTaskHandle task_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskType(MaaToolKitTaskHandle task_handle, MaaString new_type);
-    MaaString MAA_TOOLKIT_API MaaToolKitGetTaskParam(MaaToolKitTaskHandle task_handle);
-    MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskParam(MaaToolKitTaskHandle task_handle, MaaString new_param);
+    MaaJsonString MAA_TOOLKIT_API MaaToolKitGetTaskParam(MaaToolKitTaskHandle task_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskParam(MaaToolKitTaskHandle task_handle, MaaJsonString new_param);
     MaaBool MAA_TOOLKIT_API MaaToolKitGetTaskEnabled(MaaToolKitTaskHandle task_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskEnabled(MaaToolKitTaskHandle task_handle, MaaBool new_enabled);
     MaaBool MAA_TOOLKIT_API MaaToolKitGetTaskIndex(MaaToolKitTaskHandle task_handle);
     MaaBool MAA_TOOLKIT_API MaaToolKitSetTaskIndex(MaaToolKitTaskHandle task_handle, MaaBool new_enabled);
 
-    MaaBool MAA_TOOLKIT_API MaaToolKitPostTask(MaaToolKitConfigHandle config_handle);
-    MaaStatus MAA_TOOLKIT_API MaaToolKitWaitTask(MaaToolKitConfigHandle config_handle);
-    MaaStatus MAA_TOOLKIT_API MaaToolKitTaskStatus(MaaToolKitConfigHandle config_handle);
-    MaaBool MAA_TOOLKIT_API MaaToolKitStopTask(MaaToolKitConfigHandle config_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitPostAllTask(MaaToolKitConfigHandle config_handle);
+    MaaStatus MAA_TOOLKIT_API MaaToolKitWaitAllTask(MaaToolKitConfigHandle config_handle);
+    MaaBool MAA_TOOLKIT_API MaaToolKitStopAllTask(MaaToolKitConfigHandle config_handle);
 
-    MaaResourceHandle MAA_TOOLKIT_API MaaToolKitGetFrameworkResource(MaaToolKitConfigHandle config_handle);
-    MaaControllerHandle MAA_TOOLKIT_API MaaToolKitGetFrameworkController(MaaToolKitConfigHandle config_handle);
-    MaaInstanceHandle MAA_TOOLKIT_API MaaToolKitGetFrameworkInstance(MaaToolKitConfigHandle config_handle);
+    MaaStatus MAA_TOOLKIT_API MaaToolKitTaskStatus(MaaToolKitTaskHandle task_handle);
+
+    MaaResourceHandle MAA_TOOLKIT_API MaaToolKitGetRawResource(MaaToolKitConfigHandle config_handle);
+    MaaControllerHandle MAA_TOOLKIT_API MaaToolKitGetRawController(MaaToolKitConfigHandle config_handle);
+    MaaInstanceHandle MAA_TOOLKIT_API MaaToolKitGetRawInstance(MaaToolKitConfigHandle config_handle);
 
     MaaBool MAA_TOOLKIT_API MaaToolKitStartWebServer(MaaString ip, uint16_t port);
     MaaBool MAA_TOOLKIT_API MaaToolKitStopWebServer();
