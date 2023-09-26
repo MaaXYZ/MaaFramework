f37556b071776af75714bbe7557dafb44da7f587
nekosu <liao.hy@outlook.com>
2023-09-27 00:30:48 +0800

    feat: update ci

--- update-proto.bat
+++ update-proto.bat
@@ -3,19 +3,11 @@
 set PROTOC=.\MaaDeps\vcpkg\installed\x64-windows\tools\protobuf\protoc
 set GRPC_PLUGIN=.\MaaDeps\vcpkg\installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe
 
-set INDIR=.\source\MaaRpc
+set INDIR=.\include\MaaFramework\interfaces
 set OUTDIR=.\source\MaaRpc\generated
 
-
-set CURSRC=Types.proto
-call :BUILD
-set CURSRC=Framework.proto
-call :BUILD
-
-exit
-
-:BUILD
-echo PROCESSING %INDIR%\%CURSRC%
-%PROTOC% --proto_path=%INDIR% %INDIR%/%CURSRC% --cpp_out=%OUTDIR%
-%PROTOC% --proto_path=%INDIR% %INDIR%/%CURSRC% --grpc_out=%OUTDIR% --plugin=protoc-gen-grpc=%GRPC_PLUGIN%
-goto :EOF
+for %%s in (%INDIR%\*.proto) do (
+  echo PROCESSING %%s
+  %PROTOC% --proto_path=%INDIR% %%s --cpp_out=%OUTDIR%
+  %PROTOC% --proto_path=%INDIR% %%s --grpc_out=%OUTDIR% --plugin=protoc-gen-grpc=%GRPC_PLUGIN%
+)
