8a8f70460d3e9b74e96962a5342201694113be68
MistEO <mistereo@hotmail.com>
2023-09-30 00:45:03 +0800

    chore: 移除toolkit中已废弃的实现

--- source/MaaToolKit/Server/Framework/Controller.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../ApiDispatcher.h"
-
-MAA_TOOLKIT_SERVER_NS_BEGIN
-
-void init_maa_framework_controller(ApiDispatcher& disp);
-
-MAA_TOOLKIT_SERVER_NS_END
--- source/MaaToolKit/Server/Framework/Init.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../ApiDispatcher.h"
-
-MAA_TOOLKIT_SERVER_NS_BEGIN
-
-void init_maa_framework(ApiDispatcher& disp);
-
-MAA_TOOLKIT_SERVER_NS_END
