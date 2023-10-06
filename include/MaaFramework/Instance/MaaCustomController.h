bed2975fc3f3497f8d3098ec3eae1442a6c6748e
MistEO <mistereo@hotmail.com>
2023-10-06 17:38:38 +0800

    chore: 一些参数重命名

--- include/MaaFramework/Instance/MaaCustomController.h
+++ include/MaaFramework/Instance/MaaCustomController.h
@@ -10,24 +10,26 @@ extern "C"
 
     struct MaaCustomControllerAPI
     {
-        MaaBool (*set_option)(MaaCtrlOption key, MaaStringView value, MaaTransparentArg arg);
+        MaaBool (*set_option)(MaaCtrlOption key, MaaStringView value, MaaTransparentArg handle_arg);
 
-        MaaBool (*connect)(MaaTransparentArg arg);
-        MaaBool (*click)(int32_t x, int32_t y, MaaTransparentArg arg);
-        MaaBool (*swipe)(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t duration, MaaTransparentArg arg);
-        MaaBool (*press_key)(int32_t keycode, MaaTransparentArg arg);
+        MaaBool (*connect)(MaaTransparentArg handle_arg);
+        MaaBool (*click)(int32_t x, int32_t y, MaaTransparentArg handle_arg);
+        MaaBool (*swipe)(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t duration,
+                         MaaTransparentArg handle_arg);
+        MaaBool (*press_key)(int32_t keycode, MaaTransparentArg handle_arg);
 
-        MaaBool (*touch_down)(int32_t contact, int32_t x, int32_t y, int32_t pressure, MaaTransparentArg arg);
-        MaaBool (*touch_move)(int32_t contact, int32_t x, int32_t y, int32_t pressure, MaaTransparentArg arg);
-        MaaBool (*touch_up)(int32_t contact, MaaTransparentArg arg);
+        MaaBool (*touch_down)(int32_t contact, int32_t x, int32_t y, int32_t pressure, MaaTransparentArg handle_arg);
+        MaaBool (*touch_move)(int32_t contact, int32_t x, int32_t y, int32_t pressure, MaaTransparentArg handle_arg);
+        MaaBool (*touch_up)(int32_t contact, MaaTransparentArg handle_arg);
 
-        MaaBool (*start_app)(MaaStringView package_name, MaaTransparentArg arg);
-        MaaBool (*stop_app)(MaaStringView package_name, MaaTransparentArg arg);
+        MaaBool (*start_app)(MaaStringView package_name, MaaTransparentArg handle_arg);
+        MaaBool (*stop_app)(MaaStringView package_name, MaaTransparentArg handle_arg);
 
-        MaaBool (*get_resolution)(MaaTransparentArg arg, /* out */ int32_t* width, /* out */ int32_t* height);
+        MaaBool (*get_resolution)(MaaTransparentArg handle_arg, /* out */ int32_t* width,
+                                  /* out */ int32_t* height);
 
-        MaaBool (*get_image)(MaaTransparentArg arg, /* out */ MaaImageBufferHandle buffer);
-        MaaBool (*get_uuid)(MaaTransparentArg arg, /* out */ MaaStringBufferHandle buffer);
+        MaaBool (*get_image)(MaaTransparentArg handle_arg, /* out */ MaaImageBufferHandle buffer);
+        MaaBool (*get_uuid)(MaaTransparentArg handle_arg, /* out */ MaaStringBufferHandle buffer);
     };
 
 #ifdef __cplusplus
