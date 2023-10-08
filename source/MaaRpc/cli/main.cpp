d0f8a66f204249310498aae456b1f09b7983f95f
nekosu <liao.hy@outlook.com>
2023-10-08 14:10:06 +0800

    feat: optimize MaaRpcCli

--- source/MaaRpc/cli/main.cpp
+++ source/MaaRpc/cli/main.cpp
@@ -1,10 +1,55 @@
 #include "MaaRpc/MaaRpc.h"
+#include <csignal>
 #include <iostream>
+#include <mutex>
 
-int main()
+std::mutex quit;
+bool quite = false;
+
+void sig_handler(int)
 {
-    std::string server_address("0.0.0.0:8080");
+    if (!quite) {
+        std::cout << "Quit from interrupt" << std::endl;
+    }
+    quit.unlock();
+}
+
+int main(int argc, char* argv[])
+{
+    std::string host = "0.0.0.0";
+    int port = 8080;
+
+    for (int i = 1; i < argc; i++) {
+        std::string opt = argv[i];
+        if (opt == "-v" || opt == "--version") {
+            std::cout << "MaaRpcCli version " << MAA_VERSION << std::endl;
+            return 0;
+        }
+        else if (opt == "-h" || opt == "--help") {
+            std::cout << "Usage: " << argv[0] << " [-q] [-p=PORT] [-h=HOST]" << std::endl;
+            return 0;
+        }
+        else if (opt == "-q") {
+            quite = true;
+        }
+        else if (opt.starts_with("-p=")) {
+            port = atoi(opt.c_str() + 3);
+        }
+        else if (opt.starts_with("-h=")) {
+            host = opt.substr(3);
+        }
+        else {
+            std::cerr << "Unknown option " << opt << std::endl;
+            return -1;
+        }
+    }
+    std::string server_address(host + ":" + std::to_string(port));
     MaaRpcStart(server_address.c_str());
-    std::cout << "Server listening on " << server_address << std::endl;
-    MaaRpcWait();
+    signal(SIGINT, sig_handler);
+    quit.lock();
+    if (!quite) {
+        std::cout << "Server listening on " << server_address << std::endl;
+    }
+    quit.lock();
+    MaaRpcStop();
 }
