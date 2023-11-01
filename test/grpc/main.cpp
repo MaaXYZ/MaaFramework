cb166792dcd22dcf9c963251863f2564a5868176
MistEO <mistereo@hotmail.com>
2023-11-01 15:48:02 +0800

    refactor: 拆分GPRC相关项目构建系统

--- test/grpc/main.cpp
+++ test/grpc/main.cpp
@@ -1,17 +1,21 @@
-#include "../generated/utility.grpc.pb.h"
+#include <iostream>
+
 #include <grpcpp/channel.h>
 #include <grpcpp/client_context.h>
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
-#include <iostream>
+
+#include "utility.grpc.pb.h"
 
 int main()
 {
-    auto channel = grpc::CreateChannel("0.0.0.0:8080", grpc::InsecureChannelCredentials());
+    auto channel = grpc::CreateChannel("localhost:8080", grpc::InsecureChannelCredentials());
     std::unique_ptr<::maarpc::Utility::Stub> stub(::maarpc::Utility::NewStub(channel));
     grpc::ClientContext ctx;
     ::maarpc::EmptyRequest req;
     ::maarpc::StringResponse res;
     stub->version(&ctx, req, &res);
     std::cout << res.str() << std::endl;
+
+    return 0;
 }
