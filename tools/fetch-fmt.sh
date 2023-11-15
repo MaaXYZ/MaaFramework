992b7aa1ee429eabba34ce815836dc4039a2ed62
MistEO <mistereo@hotmail.com>
2023-11-15 17:17:19 +0800

    fix: fetch-fmt 脚本在路径有空格时处理错误

--- tools/fetch-fmt.sh
+++ tools/fetch-fmt.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 FmtVersion=10.1.1
-InstallDir=$(dirname $(readlink -f $0))"/../3rdparty/include"
-WorkingDir=$(dirname $(readlink -f $0))"/../3rdparty/src/fmt"
+BaseDir=$(dirname "$(readlink -f $0)")
+InstallDir=${BaseDir}/../3rdparty/include
+WorkingDir=${BaseDir}/../3rdparty/src/fmt
 echo "FmtVersion: ${FmtVersion}, InstallDir: ${InstallDir}, WorkingDir: ${WorkingDir}"
 
-mkdir -p ${WorkingDir}
-wget https://github.com/fmtlib/fmt/releases/download/${FmtVersion}/fmt-${FmtVersion}.zip -O ${WorkingDir}/fmt.zip
-unzip ${WorkingDir}/fmt.zip -d ${WorkingDir}
-cp -r ${WorkingDir}/fmt-${FmtVersion}/include/fmt ${InstallDir}
+mkdir -p "${WorkingDir}"
+wget https://github.com/fmtlib/fmt/releases/download/${FmtVersion}/fmt-${FmtVersion}.zip -O "${WorkingDir}/fmt.zip"
+unzip "${WorkingDir}/fmt.zip" -d "${WorkingDir}"
+cp -r "${WorkingDir}/fmt-${FmtVersion}/include/fmt" "${InstallDir}"
