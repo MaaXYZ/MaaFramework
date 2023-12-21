1448bf6575edde06634356d4c3ab7ab7c2155bee
MistEO <mistereo@hotmail.com>
2023-12-21 10:42:26 +0800

    chore: sturcture main

--- sample/python/__main__.py
+++ sample/python/__main__.py
@@ -10,25 +10,6 @@ from maa.custom_action import CustomAction
 import asyncio
 
 
-class MyRecognizer(CustomRecognizer):
-    def analyze(
-        self, context, image, task_name, custom_param
-    ) -> (bool, (int, int, int, int), str):
-        return True, (0, 0, 100, 100), "Hello World!"
-
-
-class MyAction(CustomAction):
-    def run(self, context, task_name, custom_param, box, rec_detail) -> bool:
-        return True
-
-    def stop(self) -> None:
-        pass
-
-
-my_rec = MyRecognizer()
-my_act = MyAction()
-
-
 async def main():
     version = Library.open("bin")
     print(f"MaaFw Version: {version}")
@@ -63,6 +44,25 @@ async def main():
     maa_inst.register_action("MyAct", my_act)
 
     await maa_inst.run_task("StartUpAndClickButton", {})
+
+
+class MyRecognizer(CustomRecognizer):
+    def analyze(
+        self, context, image, task_name, custom_param
+    ) -> (bool, (int, int, int, int), str):
+        return True, (0, 0, 100, 100), "Hello World!"
+
+
+class MyAction(CustomAction):
+    def run(self, context, task_name, custom_param, box, rec_detail) -> bool:
+        return True
+
+    def stop(self) -> None:
+        pass
+
+
+my_rec = MyRecognizer()
+my_act = MyAction()
 
 
 if __name__ == "__main__":
