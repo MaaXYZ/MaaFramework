b7afa0ab3c8231e0f7e762c22730f1ca64d8e662
企鹅2035 <qie2035@qq.com>
2024-01-10 13:23:48 +0800

    fix: 修复 imwrite 中路径判断的问题 (#130)

--- source/include/Utils/ImageIo.h
+++ source/include/Utils/ImageIo.h
@@ -24,7 +24,8 @@ inline cv::Mat imread(const std::string& utf8_path, int flags = cv::IMREAD_COLOR
 
 inline bool imwrite(const std::filesystem::path& path, cv::InputArray img)
 {
-    if (path.has_parent_path() && !std::filesystem::create_directories(path.parent_path())) {
+    if (path.has_parent_path() && !std::filesystem::exists(path.parent_path())
+        && !std::filesystem::create_directories(path.parent_path())) {
         return false;
     }
 
