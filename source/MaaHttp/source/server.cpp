35fb21e0464bac1d0351d29a446156254b02a962
nekosu <liao.hy@outlook.com>
2024-03-15 17:34:33 +0800

    refactor: move sources

--- source/MaaHttp/source/server.cpp
+++ source/MaaHttp/source/server.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
-
-#include "info.hpp"
-#include "utils/phony.hpp"
-
+#include "server.h"
 #include "spec/spec.hpp"
 
-int main()
+void setup_server(
+    unsigned short port,
+    lhg::server::Server*& server,
+    lhg::server::Dispatcher*& dispatcher)
 {
-    auto* dispatcher = new lhg::server::Dispatcher();
-    auto* server = new lhg::server::Server(dispatcher, 13126);
+    dispatcher = new lhg::server::Dispatcher();
+    server = new lhg::server::Server(dispatcher, 13126);
 
     lhg::server::apply_function<lhg::maa::__function_list>(dispatcher);
     lhg::server::apply_callback<lhg::maa::__callback_list>(dispatcher);
     lhg::server::apply_handle<lhg::maa::__handle_list>(dispatcher);
 
     dispatcher->setup_help("/help", "maa http", "1.0.0");
-
-    server->sync_run();
-
-    return 0;
 }
