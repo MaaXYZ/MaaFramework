80e6faf69672be3f176dd4d7e370665eadc5d040
nekosu <liao.hy@outlook.com>
2024-03-22 21:36:22 +0800

    refactor: merge code, move macro

--- source/MaaHttp/source/spec/callback_forward.hpp
+++ source/MaaHttp/source/spec/callback_forward.hpp
@@ -1,25 +1,15 @@
 #pragma once
 
-#include "./utils.hpp"
-
-#include "MaaFramework/MaaDef.h"
 #include "utils/phony.hpp"
 
 #include "../info.hpp"
 
-namespace lhg::callback
-{
-
-__CALLBACK_DECLARE_OUTER_STATE(
+LHGArgOuterState(
     maa::func_type_CustomActionRun::_0_sync_context,
-    HandleManager<MaaSyncContextHandle>::ScopedHandle)
-
-__CALLBACK_DECLARE_OUTER_STATE(
+    HandleManager<MaaSyncContextHandle>::ScopedHandle);
+LHGArgOuterState(
     maa::func_type_CustomRecognizerAnalyze::_0_sync_context,
-    HandleManager<MaaSyncContextHandle>::ScopedHandle)
-
-__CALLBACK_DECLARE_OUTER_STATE(
+    HandleManager<MaaSyncContextHandle>::ScopedHandle);
+LHGArgOuterState(
     maa::func_type_CustomRecognizerAnalyze::_1_image,
-    HandleManager<MaaImageBufferHandle>::ScopedHandle)
-
-}; // namespace lhg::callback
+    HandleManager<MaaImageBufferHandle>::ScopedHandle);
