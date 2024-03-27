6c80d4fd1f776a5fb6ae44612efba03dbd1e0b02
nekosu <liao.hy@outlook.com>
2024-03-28 01:24:30 +0800

    refactor: split main

--- source/MaaHttp/source/main.cpp
+++ source/MaaHttp/source/main.cpp
@@ -11,6 +11,41 @@
 
 #include "server.h"
 
+struct LaunchInfo
+{
+    unsigned short port = 0;
+    bool slave = false;
+};
+
+void parseArgs(int argc, char* argv[], LaunchInfo& info)
+{
+    for (int i = 1; i < argc; i++) {
+        if (i + 1 < argc && !strcmp(argv[i], "-p")) {
+            info.port = static_cast<unsigned short>(atoi(argv[i + 1]));
+            if (info.port > 65535) {
+                std::cerr << "invalid port " << argv[i] << std::endl;
+                info.port = 0;
+            }
+        }
+        else if (!strcmp(argv[i], "-s")) {
+            info.slave = true;
+        }
+    }
+}
+
+void slaveMode(unsigned short port)
+{
+    lhg::server::Dispatcher* dispatcher;
+    lhg::server::Server* server;
+    setup_server(port, server, dispatcher);
+
+    port = server->port();
+
+    std::cout << port << std::endl;
+
+    server->sync_run();
+}
+
 struct SlaveInfo
 {
     std::shared_ptr<boost::process::child> child;
@@ -34,99 +69,137 @@ struct SlaveInfo
     }
 };
 
-int main(int argc, char* argv[])
+struct MasterMode
 {
-    unsigned port = 0;
-    bool slave = false;
-    for (int i = 1; i < argc; i++) {
-        if (i + 1 < argc && !strcmp(argv[i], "-p")) {
-            port = atoi(argv[i + 1]);
-            if (port > 65535) {
-                std::cerr << "invalid port " << argv[i] << std::endl;
-                port = 0;
+    bool running = true;
+    std::mutex mtx;
+    std::map<unsigned short, SlaveInfo> childs;
+    std::thread clean_thread;
+
+    MasterMode()
+        : clean_thread([this]() { clean(); })
+    {
+    }
+
+    ~MasterMode()
+    {
+        running = false;
+        clean_thread.join();
+    }
+
+    void clean()
+    {
+        while (running) {
+            std::unique_lock<std::mutex> lock(mtx);
+
+            for (auto it = childs.begin(); it != childs.end();) {
+                if (it->second.isTimeout()) {
+                    std::cerr << "kill slave at " << it->first << std::endl;
+                    it->second.child->terminate();
+                    it->second.stdout_processer->join();
+                    it = childs.erase(it);
+                }
+                else {
+                    it++;
+                }
             }
-        }
-        else if (!strcmp(argv[i], "-s")) {
-            slave = true;
+            lock.unlock();
+
+            std::this_thread::sleep_for(std::chrono::seconds(60));
         }
     }
 
-    if (slave) {
-        lhg::server::Dispatcher* dispatcher;
-        lhg::server::Server* server;
-        setup_server(static_cast<unsigned short>(port), server, dispatcher);
+    std::optional<unsigned short> start()
+    {
+        auto self = boost::dll::program_location();
 
-        port = server->port();
+        boost::process::ipstream out_stream;
+        auto child = std::make_shared<boost::process::child>(
+            self,
+            std::vector<std::string> { "-s" },
+            boost::process::std_out > out_stream);
 
-        std::cout << port << std::endl;
+        std::string slave_port_row;
+        std::getline(out_stream, slave_port_row);
+        unsigned short slave_port = static_cast<unsigned short>(std::stoul(slave_port_row));
 
-        server->sync_run();
+        if (slave_port > 0) {
+            auto stdout_processer =
+                std::make_shared<std::thread>([os = std::move(out_stream), slave_port]() mutable {
+                    std::string row;
+                    while (std::getline(os, row)) {
+                        std::cout << std::format("[{}] {}", slave_port, row) << std::endl;
+                    }
+                });
 
+            std::unique_lock<std::mutex> lock(mtx);
+
+            auto& slave = childs[slave_port];
+            slave.child = child;
+            slave.stdout_processer = stdout_processer;
+            slave.update();
+
+            return slave_port;
+        }
+        else {
+            child->terminate();
+            return std::nullopt;
+        }
+    }
+
+    bool heart(unsigned short port)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if (!childs.contains(port)) {
+            return false;
+        }
+        childs[port].update();
+        return true;
+    }
+
+    bool stop(unsigned short port)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if (!childs.contains(port)) {
+            return false;
+        }
+
+        std::cerr << "kill slave at " << port << std::endl;
+
+        childs[port].child->terminate();
+        childs[port].stdout_processer->join();
+        childs.erase(port);
+
+        return true;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    LaunchInfo info;
+    parseArgs(argc, argv, info);
+
+    if (info.slave) {
+        slaveMode(info.port);
         return 0;
     }
     else {
         auto disp = new lhg::server::Dispatcher();
-        auto server = new lhg::server::Server(disp, static_cast<unsigned short>(port));
+        auto server = new lhg::server::Server(disp, static_cast<unsigned short>(info.port));
 
-        auto self = boost::dll::program_location();
-
-        bool running = true;
-        std::mutex mtx;
-        std::map<unsigned short, SlaveInfo> childs;
-
-        std::thread cleaner([&]() {
-            while (running) {
-                std::unique_lock<std::mutex> lock(mtx);
-
-                for (auto it = childs.begin(); it != childs.end();) {
-                    if (it->second.isTimeout()) {
-                        std::cerr << "kill slave at " << it->first << std::endl;
-                        it->second.child->terminate();
-                        it->second.stdout_processer->join();
-                        it = childs.erase(it);
-                    }
-                    else {
-                        it++;
-                    }
-                }
-                lock.unlock();
-
-                std::this_thread::sleep_for(std::chrono::seconds(60));
-            }
-        });
+        MasterMode master;
 
         disp->handle(
             "/start",
             [&](auto&, auto& res, const auto& req) {
-                boost::process::ipstream out_stream;
-                auto child = std::make_shared<boost::process::child>(
-                    self,
-                    std::vector<std::string> { "-s" },
-                    boost::process::std_out > out_stream);
-
-                std::string slave_port_row;
-                std::getline(out_stream, slave_port_row);
-                unsigned short slave_port = static_cast<unsigned short>(std::stoul(slave_port_row));
-                if (slave_port > 0) {
-                    auto stdout_processer =
-                        std::make_shared<std::thread>([os = std::move(out_stream), port]() mutable {
-                            std::string row;
-                            while (std::getline(os, row)) {
-                                std::cout << std::format("[{}] {}", port, row) << std::endl;
-                            }
-                        });
-
-                    std::unique_lock<std::mutex> lock(mtx);
-
-                    std::cerr << "start slave at " << slave_port << std::endl;
-                    auto& slave = childs[slave_port];
-                    slave.child = child;
-                    slave.stdout_processer = stdout_processer;
-                    slave.update();
-                    res = { { "port", slave_port } };
+                auto port = master.start();
+                if (port.has_value()) {
+                    std::cerr << "start slave at " << port.value() << std::endl;
+                    res = { { "port", port.value() } };
                 }
                 else {
-                    child->terminate();
                     res = { { "port", json::value() } };
                 }
             },
@@ -135,55 +208,48 @@ int main(int argc, char* argv[])
         disp->handle(
             "/heart",
             [&](auto&, auto& res, const auto& req) {
-                std::unique_lock<std::mutex> lock(mtx);
-
                 if (!req.contains("port")) {
                     res = { { "success", false }, { "error", "port not provided" } };
                     return;
                 }
                 unsigned short slave_port =
                     static_cast<unsigned short>(req.at("port").as_unsigned());
-                if (!childs.contains(slave_port)) {
+                if (master.heart(slave_port)) {
+                    res = { { "success", true } };
+                }
+                else {
                     res = { { "success", false },
                             { "error", std::format("{} not exists", slave_port) } };
-                    return;
                 }
-                childs[slave_port].update();
-                res = { { "success", true } };
             },
             [](auto&, auto&) {});
 
         disp->handle(
             "/stop",
             [&](auto&, auto& res, const auto& req) {
-                std::unique_lock<std::mutex> lock(mtx);
-
-                unsigned short port = static_cast<unsigned short>(req.at("port").as_unsigned());
-                if (!childs.contains(port)) {
-                    res = { { "success", false }, { "error", std::format("{} not exists", port) } };
+                if (!req.contains("port")) {
+                    res = { { "success", false }, { "error", "port not provided" } };
                     return;
                 }
-                std::cerr << "kill slave at " << port << std::endl;
-                childs[port].child->terminate();
-                childs[port].stdout_processer->join();
-                childs.erase(port);
-                res = { { "success", true } };
+                unsigned short port = static_cast<unsigned short>(req.at("port").as_unsigned());
+                if (master.stop(port)) {
+                    std::cerr << "kill slave at " << port << std::endl;
+                    res = { { "success", true } };
+                }
+                else {
+                    res = { { "success", false }, { "error", std::format("{} not exists", port) } };
+                }
             },
             [](auto&, auto&) {});
 
-        port = server->port();
+        auto port = server->port();
 
         server->run();
 
         std::cout << "deamon started at 127.0.0.1:" << port << std::endl;
-
         std::cin.get();
 
-        running = true;
-
         server->stop();
-
-        cleaner.join();
 
         return 0;
     }
