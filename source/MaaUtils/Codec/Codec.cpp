5aedb8792dab3a43a97dd910fd7b034d7ba2be4b
MistEO <mistereo@hotmail.com>
2024-04-03 16:57:26 +0800

    styles: format

--- source/MaaUtils/Codec/Codec.cpp
+++ source/MaaUtils/Codec/Codec.cpp
@@ -7,12 +7,13 @@ MAA_NS_BEGIN
 std::wstring to_u16(const std::string& u8str)
 {
 #ifdef _WIN32
-    const std::codecvt<wchar_t, char, std::mbstate_t>& cvt = std::use_facet<
-        std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale(
-        ".65001")); // 美国(英语,UTF-8)，https://learn.microsoft.com/en-us/cpp/c-runtime-library/locale-names-languages-and-country-region-strings?view=msvc-170
-#else
+    // 美国(英语,UTF-8)，https://learn.microsoft.com/en-us/cpp/c-runtime-library/locale-names-languages-and-country-region-strings?view=msvc-170
     const std::codecvt<wchar_t, char, std::mbstate_t>& cvt =
-        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale("C.UTF-8")); // 默认字符集(UTF-8)
+        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale(".65001"));
+#else
+    // 默认字符集(UTF-8)
+    const std::codecvt<wchar_t, char, std::mbstate_t>& cvt =
+        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale("C.UTF-8"));
 #endif
     std::mbstate_t mb {};
     std::wstring t(u8str.length(), L'\0');
@@ -31,19 +32,26 @@ std::wstring to_u16(const std::string& u8str)
 std::string from_u16(const std::wstring& u16str)
 {
 #ifdef _WIN32
-    const std::codecvt<wchar_t, char, std::mbstate_t>& cvt = std::use_facet<
-        std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale(
-        ".65001")); // 美国(英语,UTF-8)，https://learn.microsoft.com/en-us/cpp/c-runtime-library/locale-names-languages-and-country-region-strings?view=msvc-170
-#else
+    // 美国(英语,UTF-8)，https://learn.microsoft.com/en-us/cpp/c-runtime-library/locale-names-languages-and-country-region-strings?view=msvc-170
     const std::codecvt<wchar_t, char, std::mbstate_t>& cvt =
-        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale("C.UTF-8")); // 默认字符集(UTF-8)
+        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale(".65001"));
+#else
+    // 默认字符集(UTF-8)
+    const std::codecvt<wchar_t, char, std::mbstate_t>& cvt =
+        std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale("C.UTF-8"));
 #endif
     std::mbstate_t mb {};
     std::string t(u16str.length() * cvt.max_length(), L'\0');
     const wchar_t* from_next = nullptr;
     char* to_next = nullptr;
-    std::codecvt_base::result result =
-        cvt.out(mb, &u16str[0], &u16str[u16str.length()], from_next, &t[0], &t[t.length()], to_next);
+    std::codecvt_base::result result = cvt.out(
+        mb,
+        &u16str[0],
+        &u16str[u16str.length()],
+        from_next,
+        &t[0],
+        &t[t.length()],
+        to_next);
     if (result != std::codecvt_base::ok) {
         return std::string(u16str.length(), '?');
     }
