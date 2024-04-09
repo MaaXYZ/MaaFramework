4db3df773d7c75da888e54c9c677e31b5114ea83
MistEO <mistereo@hotmail.com>
2024-04-09 15:38:59 +0800

    fix: 简单糊一下sleep时间错误

--- source/MaaFramework/Task/Actuator.cpp
+++ source/MaaFramework/Task/Actuator.cpp
@@ -250,22 +250,9 @@ void Actuator::sleep(unsigned ms) const
 
 void Actuator::sleep(std::chrono::milliseconds ms) const
 {
-    using namespace std::chrono_literals;
+    LogFunc << ms;
 
-    if (ms == 0ms) {
-        std::this_thread::yield();
-        return;
-    }
-
-    auto interval = std::min(ms, 5000ms);
-
-    LogTrace << "ready to sleep" << ms << VAR(interval);
-
-    for (auto sleep_time = interval; sleep_time <= ms; sleep_time += interval) {
-        std::this_thread::sleep_for(interval);
-    }
-
-    LogTrace << "end of sleep" << ms << VAR(interval);
+    std::this_thread::sleep_for(ms);
 }
 
 MAA_TASK_NS_END
