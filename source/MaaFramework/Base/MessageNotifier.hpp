1339abaa7569e172ab9f6509c7baf10518b291fa
MistEO <mistereo@hotmail.com>
2024-04-16 15:49:05 +0800

    fix: 潜在的回调生命周期问题

--- source/MaaFramework/Base/MessageNotifier.hpp
+++ source/MaaFramework/Base/MessageNotifier.hpp
@@ -25,7 +25,9 @@ public:
         if (!callback_) {
             return;
         }
-        callback_(msg.data(), details.to_string().c_str(), callback_arg_);
+
+        const std::string str_detail = details.to_string();
+        callback_(msg.data(), str_detail.c_str(), callback_arg_);
     }
 
 private:
