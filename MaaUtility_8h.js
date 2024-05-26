eb1ffcc9c3e88b25509d518e5d1cd7eb67f2f1d9
MistEO <MistEO@users.noreply.github.com>
2024-05-26 13:48:51 +0000

    (main) v1.8.0 5d59c6cda950a41fca8bbda3255806eda49331cf

--- MaaUtility_8h.js
+++ MaaUtility_8h.js
@@ -2,7 +2,7 @@ var MaaUtility_8h =
 [
     [ "MaaVersion", "MaaUtility_8h.html#a62ca053991307446cb195865497a40f1", null ],
     [ "MaaSetGlobalOption", "MaaUtility_8h.html#a1dba15d9cc61cf339c434085738f873c", null ],
-    [ "MaaQueryRecognitionDetail", "MaaUtility_8h.html#a2c2351a9b0a6c2ea9ec828154cb0b4db", null ],
-    [ "MaaQueryNodeDetail", "MaaUtility_8h.html#afc9cc1ea9d9af7b9518b4aab0ba6012f", null ],
-    [ "MaaQueryTaskDetail", "MaaUtility_8h.html#a502b61dd7a7f977c84ee65070a40fafa", null ]
+    [ "MaaQueryRecognitionDetail", "MaaUtility_8h.html#a6b547a92ca6d54841f593b8c6518e3b7", null ],
+    [ "MaaQueryNodeDetail", "MaaUtility_8h.html#a1c0038e0b38b1558e54a7f43a0d4aa3e", null ],
+    [ "MaaQueryTaskDetail", "MaaUtility_8h.html#ac93f377005950486ac1b4f8c1562c056", null ]
 ];
