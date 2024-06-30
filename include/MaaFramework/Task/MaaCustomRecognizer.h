fe0d2fed5f191c36e0a952654dc0e3d0fd71acfa
枫雨 <35213527+moomiji@users.noreply.github.com>
2024-06-30 17:04:59 +0800

    feat: 添加 index.json 构建用于 MaaApiConverter (#270)

--- include/MaaFramework/Task/MaaCustomRecognizer.h
+++ include/MaaFramework/Task/MaaCustomRecognizer.h
@@ -39,8 +39,8 @@ extern "C"
             MaaStringView task_name,
             MaaStringView custom_recognition_param,
             MaaTransparentArg recognizer_arg,
-            /*out*/ MaaRectHandle out_box,
-            /*out*/ MaaStringBufferHandle out_detail);
+            /* out */ MaaRectHandle out_box,
+            /* out */ MaaStringBufferHandle out_detail);
     };
 
 #ifdef __cplusplus
