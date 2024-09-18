82cd2efef50f8a3da0aaa2157a7ef84e5647186d
MistEO <mistereo@hotmail.com>
2024-09-18 16:27:02 +0800

    chore: rename

--- source/binding/Python/maa/job.py
+++ source/binding/Python/maa/job.py
@@ -26,22 +26,22 @@ class Status:
 
 
 class Job:
-    _maaid: MaaId
+    job_id: MaaId
 
-    def __init__(self, maaid: MaaId, status_func, wait_func):
-        self._maaid = maaid
+    def __init__(self, job_id: MaaId, status_func, wait_func):
+        self.job_id = job_id
         self._status_func = status_func
         self._wait_func = wait_func
 
     def get_id(self) -> int:
-        return int(self._maaid)
+        return int(self.job_id)
 
     def wait(self) -> "Job":
-        self._wait_func(self._maaid)
+        self._wait_func(self.job_id)
         return self
 
     def status(self) -> Status:
-        return Status(self._status_func(self._maaid))
+        return Status(self._status_func(self.job_id))
 
     def done(self) -> bool:
         return self.status().done()
@@ -60,8 +60,8 @@ class Job:
 
 
 class JobWithResult(Job):
-    def __init__(self, maaid: MaaId, status_func, wait_func, get_func):
-        super().__init__(maaid, status_func, wait_func)
+    def __init__(self, job_id: MaaId, status_func, wait_func, get_func):
+        super().__init__(job_id, status_func, wait_func)
         self._get_func = get_func
 
     def wait(self) -> "JobWithResult":
@@ -69,4 +69,4 @@ class JobWithResult(Job):
         return self
 
     def get(self):
-        return self._get_func(self._maaid)
+        return self._get_func(self.job_id)
