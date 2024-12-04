cfc0720b6a53283dd4dc75a21ea6c1efb9fa17c2
MistEO <mistereo@hotmail.com>
2024-12-04 18:23:18 +0800

    Update __init__.py

--- source/binding/Python/maa/__init__.py
+++ source/binding/Python/maa/__init__.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from .library import Library
 
-__PATH = Path(os.environ.get("MAAFW_BINARY_PATH"))
-if not __PATH:
+env_path = Path(os.environ.get("MAAFW_BINARY_PATH"))
+if env_path:
+    __PATH = env_path
+else:
     __PATH = Path(Path(__file__).parent, "bin")
 
 Library.open(__PATH)
