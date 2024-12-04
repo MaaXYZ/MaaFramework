5b0ae3259b8e60d1a28dffbe605e67a4e27884e0
不留 <weinibuliu@outlook.com>
2024-12-04 12:55:41 +0800

    chore: 修复 binding test 不通过的问题；调整报错信息

--- source/binding/Python/maa/__init__.py
+++ source/binding/Python/maa/__init__.py
@@ -1,17 +1,18 @@
 from pathlib import Path
+import sys
 
 from .library import Library
 
 __PATH = Path(Path(__file__).parent, "bin")
+if not __PATH.exists():
+    # sys.argv is designed for Github Action,we seldom use this.
+    if len(sys.argv) < 2:
+        raise FileNotFoundError(f"`{__PATH}` is not existed.")
 
-# If you want to print maafw version,please use `from maa import ver`
-ver = None
+    __PATH = Path(Path(sys.argv[1]).resolve(), "bin")
+    if not __PATH.exists():
+        raise FileNotFoundError(f"`{__PATH}` is not existed.")
 
-if __PATH.exists():
-    ver = Library.open(__PATH)
-    if not ver:
-        raise RuntimeError(
-            "Library not initialized, please call `library.open()` first."
-        )
-else:
-    raise FileNotFoundError(f"{__PATH} is not exists.")
+ver = Library.open(__PATH)
+if not ver:
+    raise RuntimeError("Fail to open the library.")
