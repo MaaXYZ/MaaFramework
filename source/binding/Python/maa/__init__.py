b688c6009c1577a6e0a8bc8aa4156cf2ba74d31d
不留 <weinibuliu@outlook.com>
2024-12-04 18:19:34 +0800

    fix: 修复获取系统变量时未格式化的问题

--- source/binding/Python/maa/__init__.py
+++ source/binding/Python/maa/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from .library import Library
 
-__PATH = os.environ.get("MAAFW_BINARY_PATH")
+__PATH = Path(os.environ.get("MAAFW_BINARY_PATH"))
 if not __PATH:
     __PATH = Path(Path(__file__).parent, "bin")
 
