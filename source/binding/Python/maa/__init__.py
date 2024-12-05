6001df015d2f73adacef0b670ccfead7660cfa28
不留 <weinibuliu@outlook.com>
2024-12-05 13:06:44 +0800

    fix: 修复因系统变量不存在引发的 Python Binding 异常 (#449)

--- source/binding/Python/maa/__init__.py
+++ source/binding/Python/maa/__init__.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from .library import Library
 
-env_path = Path(os.environ.get("MAAFW_BINARY_PATH"))
+env_path = os.environ.get("MAAFW_BINARY_PATH")
 if env_path:
-    __PATH = env_path
+    __PATH = Path(env_path)
 else:
     __PATH = Path(Path(__file__).parent, "bin")
 
