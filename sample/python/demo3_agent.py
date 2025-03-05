84ea50ec6f7079641b6656ced17e2c5881f05687
MistEO <mistereo@hotmail.com>
2025-03-05 22:56:55 +0800

    chore: fix py sample

--- sample/python/demo3_agent.py
+++ sample/python/demo3_agent.py
@@ -1,3 +1,5 @@
+import sys
+
 from maa.agent.agent_server import AgentServer
 from maa.custom_recognition import CustomRecognition
 from maa.custom_action import CustomAction
@@ -5,8 +7,12 @@ from maa.context import Context
 
 
 def main():
-    AgentServer.start_up()
+
+    socket_id = sys.argv[-1]
+
+    AgentServer.start_up(socket_id)
     AgentServer.join()
+    AgentServer.shut_down()
 
 
 @AgentServer.custom_recognition("MyRecongition")
