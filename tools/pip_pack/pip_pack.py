b974e808497aa66e3bd8f239bd8d0a2de914baef
Lumina <starry.qvq@gmail.com>
2025-03-08 01:49:58 +0800

    refactor: polish pip pack (#570)
    改动概述：把python打包步骤的工作目录迁移到顶层目录下的`./pip_pack`里，这样就不会和源码目录交叉污染了。此外还用pathlib重构了一下脚本，提升可读性。
    
    关联issue： #569 
    自测：https://github.com/lumina37/MaaFramework/actions/runs/13718436187

--- tools/pip_pack/pip_pack.py
+++ tools/pip_pack/pip_pack.py
@@ -1,107 +1,83 @@
-import os
+from __future__ import annotations
+
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 import tomlkit
 
-IGNORES = ["MaaPiCli*", "MaaNode*"]
+SRC_DIR = Path("source/binding/Python")
+PACK_DIR = Path("pip_pack")
+WHL_DIR = PACK_DIR / "wheel"
+DIST_DIR = PACK_DIR / "dist"
+
+
+def copy_base():
+    print("Copying base...", end="")
+
+    if not PACK_DIR.exists():
+        shutil.copytree(SRC_DIR, PACK_DIR)
+        with (PACK_DIR / ".gitignore").open("w") as f:
+            f.write("*")
+
+    shutil.copy("README.md", PACK_DIR)
+    shutil.copy("README_en.md", PACK_DIR)
+    shutil.copy("LICENSE.md", PACK_DIR)
+
+    print("done")
 
 
 def set_toml_ver():
-    TOML_FILE = "pyproject.toml"
-
-    version = sys.argv[1]
-    if "ci" in version:
-        base_ver, ci_ver = version.split("ci")
+    git_tag = sys.argv[1]
+    if "ci" in git_tag:
+        base_ver, ci_ver = git_tag.split("ci")
         version = base_ver + "dev" + ci_ver.split("-")[0]
 
-    print("Setting version to:", version)
+    print(f"Setting version to: {version}")
 
-    with open(TOML_FILE, "r") as f:
+    toml_path = PACK_DIR / "pyproject.toml"
+    with toml_path.open() as f:
         py_project = tomlkit.load(f)
         py_project["project"]["version"] = version
 
-    with open(TOML_FILE, "w") as f:
+    with toml_path.open("w") as f:
         tomlkit.dump(py_project, f)
-
-
-WHL_DIR = "wheel"
-DIST_DIR = "dist"
-SRC_DIR = "src"
-
-
-def clean_cwd():
-    print("Cleaning working dir...")
-
-    for root, dirs, files in os.walk(os.getcwd()):
-        for dir_name in dirs:
-            if (
-                dir_name
-                in (
-                    SRC_DIR,
-                    DIST_DIR,
-                    WHL_DIR,
-                    "__pycache__",
-                    ".cache",
-                    "build",
-                )
-                or ".egg-info" in dir_name
-            ):
-
-                shutil.rmtree(os.path.join(root, dir_name))
-                print("    Removing", dir_name)
-        for file_name in files:
-            if file_name.endswith(
-                (".pyc", ".pyo", ".whl", ".md"),
-            ):
-                os.remove(os.path.join(root, file_name))
-                print("    Removing", file_name)
-
-    print("done")
 
 
 def gen_base_whl():
     print("Generating base wheel...", end="")
-    subprocess.check_output(
+
+    subprocess.run(
         [sys.executable, "-m", "build", "-w"],
+        cwd=PACK_DIR,
+        check=True,
     )
+
     print("done")
 
 
-def unpack_base_whl() -> str:
-    """Return: unpack path"""
-
+def unpack_base_whl() -> tuple[Path, Path]:
     print("Unpacking base wheel...", end="")
 
-    whl_name = os.listdir(DIST_DIR)[0]
-    subprocess.check_output(
-        [sys.executable, "-m", "wheel", "unpack", whl_name],
+    whl_path = next(DIST_DIR.glob("*.whl"))
+    subprocess.run(
+        [sys.executable, "-m", "wheel", "unpack", whl_path.name],
         cwd=DIST_DIR,
+        check=True,
     )
 
-    for file in os.listdir(DIST_DIR):
-        unpack_path = os.path.join(DIST_DIR, file)
-        if os.path.isdir(unpack_path):
-            unpack_dir = file
-            break
+    # Pick the first matching obj from the generator
+    unpack_dir = next(d for d in DIST_DIR.iterdir() if d.is_dir())
+    info_dir = next(d for d in unpack_dir.iterdir() if d.suffix == ".dist-info")
 
-    for file in os.listdir(unpack_path):
-        if ".dist-info" in file:
-            info_dir = file
-            break
-
-    info_path = os.path.join(unpack_path, info_dir)
-    os.remove(os.path.join(info_path, "RECORD"))
+    (info_dir / "RECORD").unlink()
 
     print("done")
-    return unpack_dir, info_path
+    return unpack_dir, info_dir
 
 
-ASSETS_PATH = os.path.join("..", "..", "assets")
-
-
-def pack(pack_dir: str, info_path: str):
+def pack(pack_dir: Path, info_dir: Path):
     WHL_TAGS = {
         "win-x86_64": "win_amd64",
         "win-aarch64": "win_arm64",
@@ -113,34 +89,34 @@ def pack(pack_dir: str, info_path: str):
         # "android-aarch64": "android_arm64_v8a",
     }
 
-    os.mkdir(WHL_DIR)
-
-    wheel_path = os.path.join(info_path, "WHEEL")
+    WHL_DIR.mkdir(exist_ok=True)
+    pack_bin_path = pack_dir / "maa/bin"
 
     def edit_whl_tag(whl_tag: str):
         print(f"    Editing wheel tag to: {whl_tag}...", end="")
 
+        wheel_path = info_dir / "WHEEL"
+
         lines = []
-        for line in open(wheel_path, "r"):
+        for line in wheel_path.open():
             if line.startswith("Tag:"):
-                line = "Tag: " + "py3-none-" + whl_tag
-            lines.append(line.rstrip())
-        with open(wheel_path, "w") as f:
-            f.write("\n".join(lines))
+                line = f"Tag: py3-none-{whl_tag}\n"
+            lines.append(line)
+        with wheel_path.open("w") as f:
+            f.writelines(lines)
 
         print("done")
-
-    pack_bin_path = os.path.join(DIST_DIR, pack_dir, "maa", "bin")
 
     def copy_bin(platform: str):
         print(f"    Copying bin for: {platform}...", end="")
 
-        bin_path = os.path.join(ASSETS_PATH, "MAA-" + platform, "bin")
-        shutil.copytree(
-            bin_path,
-            pack_bin_path,
-            ignore=shutil.ignore_patterns(*IGNORES),
-        )
+        bin_path = Path("assets") / f"MAA-{platform}" / "bin"
+
+        pack_bin_path.mkdir(exist_ok=True)
+        for fp in bin_path.iterdir():
+            if fp.name.startswith(("MaaPiCli", "MaaNode")):
+                continue
+            shutil.copyfile(fp, pack_bin_path / fp.name)
 
         print("done")
 
@@ -152,10 +128,11 @@ def pack(pack_dir: str, info_path: str):
     def pack_whl(whl_tag: str):
         print(f"    Packing whl for: {whl_tag}...", end="")
 
-        pack_path = os.path.join("..", DIST_DIR, pack_dir)
-        subprocess.check_output(
+        pack_path = Path("../dist") / pack_dir.name
+        subprocess.run(
             [sys.executable, "-m", "wheel", "pack", pack_path],
             cwd=WHL_DIR,
+            check=True,
         )
 
         print("done")
@@ -171,30 +148,9 @@ def pack(pack_dir: str, info_path: str):
         print("done")
 
 
-def copy_base():
-    print("Copying base...", end="")
-
-    PROJECT_PATH = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    src_path = os.path.join(PROJECT_PATH, "source", "binding", "Python")
-    shutil.copytree(src_path, SRC_DIR)
-
-    readme_path = os.path.join(PROJECT_PATH, "README.md")
-    shutil.copy(readme_path, "README.md")
-
-    readme_path = os.path.join(PROJECT_PATH, "README_en.md")
-    shutil.copy(readme_path, "README_en.md")
-
-    license_path = os.path.join(PROJECT_PATH, "LICENSE.md")
-    shutil.copy(license_path, "LICENSE.md")
-
-    print("done")
-
-
 def main():
-    clean_cwd()
-    set_toml_ver()
     copy_base()
+    set_toml_ver()
     gen_base_whl()
     paths = unpack_base_whl()
     pack(*paths)
