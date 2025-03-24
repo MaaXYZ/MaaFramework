c60bfc658c91175bcf435f339f12e1674088fd07
MistEO <mistereo@hotmail.com>
2025-03-24 15:51:47 +0800

    test: remove chars which cannot encode utf8

--- test/agent/agent_main_test.py
+++ test/agent/agent_main_test.py
@@ -74,7 +74,7 @@ def api_test():
             "custom_recognition": "MyRec",
             "action": "Custom",
             "custom_action": "MyAct",
-            "custom_action_param": "👋哈哈哈(*´▽｀)ノノ😀",
+            "custom_action_param": "哈哈哈(*´▽｀)ノノ",
         },
     }
     detail = tasker.post_task("Entry", ppover).wait().get()
