90a8b25b35168173616447aee3b6122952d0a01c
Windsland <86581225+Windsland52@users.noreply.github.com>
2025-03-26 23:25:05 +0800

    fix(python): type checking error in custom_recognition.py

--- source/binding/Python/maa/custom_recognition.py
+++ source/binding/Python/maa/custom_recognition.py
@@ -98,11 +98,11 @@ class CustomRecognition(ABC):
         elif result is None:
             return int(False)
 
-        elif (
-            isinstance(result, tuple)
-            and len(result) == 4
-            and all(isinstance(x, (int, float)) for x in result)
-        ):
+        # RectType
+        elif (isinstance(result, Rect) or 
+                (isinstance(result, list) and len(result) >= 4) or
+                (isinstance(result, numpy.ndarray) and result.size >= 4) or
+                (isinstance(result, tuple) and len(result) == 4 and all(isinstance(x, (int, float)) for x in result))):
             rect_buffer.set(result)
             return int(True)
 
