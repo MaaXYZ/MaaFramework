56e0852e76e3fccb22886071edf3057cbe0c5256
MistEO <mistereo@hotmail.com>
2025-09-09 23:12:06 +0800

    styles: format

--- source/MaaAdbControlUnit/EmulatorExtras/MuMuPlayerExtras.cpp
+++ source/MaaAdbControlUnit/EmulatorExtras/MuMuPlayerExtras.cpp
@@ -204,108 +204,108 @@ int MuMuPlayerExtras::android_keycode_to_linux_key_code(int key)
     // https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
     static const std::unordered_map<int, int> kMap = {
         // Letters
-        {29, 30},   // KEYCODE_A -> KEY_A
-        {30, 48},   // KEYCODE_B -> KEY_B
-        {31, 46},   // KEYCODE_C -> KEY_C
-        {32, 32},   // KEYCODE_D -> KEY_D
-        {33, 18},   // KEYCODE_E -> KEY_E
-        {34, 33},   // KEYCODE_F -> KEY_F
-        {35, 34},   // KEYCODE_G -> KEY_G
-        {36, 35},   // KEYCODE_H -> KEY_H
-        {37, 23},   // KEYCODE_I -> KEY_I
-        {38, 36},   // KEYCODE_J -> KEY_J
-        {39, 37},   // KEYCODE_K -> KEY_K
-        {40, 38},   // KEYCODE_L -> KEY_L
-        {41, 50},   // KEYCODE_M -> KEY_M
-        {42, 49},   // KEYCODE_N -> KEY_N
-        {43, 24},   // KEYCODE_O -> KEY_O
-        {44, 25},   // KEYCODE_P -> KEY_P
-        {45, 16},   // KEYCODE_Q -> KEY_Q
-        {46, 19},   // KEYCODE_R -> KEY_R
-        {47, 31},   // KEYCODE_S -> KEY_S
-        {48, 20},   // KEYCODE_T -> KEY_T
-        {49, 22},   // KEYCODE_U -> KEY_U
-        {50, 47},   // KEYCODE_V -> KEY_V
-        {51, 17},   // KEYCODE_W -> KEY_W
-        {52, 45},   // KEYCODE_X -> KEY_X
-        {53, 21},   // KEYCODE_Y -> KEY_Y
-        {54, 44},   // KEYCODE_Z -> KEY_Z
+        { 29, 30 }, // KEYCODE_A -> KEY_A
+        { 30, 48 }, // KEYCODE_B -> KEY_B
+        { 31, 46 }, // KEYCODE_C -> KEY_C
+        { 32, 32 }, // KEYCODE_D -> KEY_D
+        { 33, 18 }, // KEYCODE_E -> KEY_E
+        { 34, 33 }, // KEYCODE_F -> KEY_F
+        { 35, 34 }, // KEYCODE_G -> KEY_G
+        { 36, 35 }, // KEYCODE_H -> KEY_H
+        { 37, 23 }, // KEYCODE_I -> KEY_I
+        { 38, 36 }, // KEYCODE_J -> KEY_J
+        { 39, 37 }, // KEYCODE_K -> KEY_K
+        { 40, 38 }, // KEYCODE_L -> KEY_L
+        { 41, 50 }, // KEYCODE_M -> KEY_M
+        { 42, 49 }, // KEYCODE_N -> KEY_N
+        { 43, 24 }, // KEYCODE_O -> KEY_O
+        { 44, 25 }, // KEYCODE_P -> KEY_P
+        { 45, 16 }, // KEYCODE_Q -> KEY_Q
+        { 46, 19 }, // KEYCODE_R -> KEY_R
+        { 47, 31 }, // KEYCODE_S -> KEY_S
+        { 48, 20 }, // KEYCODE_T -> KEY_T
+        { 49, 22 }, // KEYCODE_U -> KEY_U
+        { 50, 47 }, // KEYCODE_V -> KEY_V
+        { 51, 17 }, // KEYCODE_W -> KEY_W
+        { 52, 45 }, // KEYCODE_X -> KEY_X
+        { 53, 21 }, // KEYCODE_Y -> KEY_Y
+        { 54, 44 }, // KEYCODE_Z -> KEY_Z
 
         // Numbers (top row)
-        {7, 11},    // KEYCODE_0 -> KEY_0
-        {8, 2},     // KEYCODE_1 -> KEY_1
-        {9, 3},     // KEYCODE_2 -> KEY_2
-        {10, 4},    // KEYCODE_3 -> KEY_3
-        {11, 5},    // KEYCODE_4 -> KEY_4
-        {12, 6},    // KEYCODE_5 -> KEY_5
-        {13, 7},    // KEYCODE_6 -> KEY_6
-        {14, 8},    // KEYCODE_7 -> KEY_7
-        {15, 9},    // KEYCODE_8 -> KEY_8
-        {16, 10},   // KEYCODE_9 -> KEY_9
+        { 7, 11 },  // KEYCODE_0 -> KEY_0
+        { 8, 2 },   // KEYCODE_1 -> KEY_1
+        { 9, 3 },   // KEYCODE_2 -> KEY_2
+        { 10, 4 },  // KEYCODE_3 -> KEY_3
+        { 11, 5 },  // KEYCODE_4 -> KEY_4
+        { 12, 6 },  // KEYCODE_5 -> KEY_5
+        { 13, 7 },  // KEYCODE_6 -> KEY_6
+        { 14, 8 },  // KEYCODE_7 -> KEY_7
+        { 15, 9 },  // KEYCODE_8 -> KEY_8
+        { 16, 10 }, // KEYCODE_9 -> KEY_9
 
         // Function keys
-        {131, 59},  // KEYCODE_F1 -> KEY_F1
-        {132, 60},  // KEYCODE_F2 -> KEY_F2
-        {133, 61},  // KEYCODE_F3 -> KEY_F3
-        {134, 62},  // KEYCODE_F4 -> KEY_F4
-        {135, 63},  // KEYCODE_F5 -> KEY_F5
-        {136, 64},  // KEYCODE_F6 -> KEY_F6
-        {137, 65},  // KEYCODE_F7 -> KEY_F7
-        {138, 66},  // KEYCODE_F8 -> KEY_F8
-        {139, 67},  // KEYCODE_F9 -> KEY_F9
-        {140, 68},  // KEYCODE_F10 -> KEY_F10
-        {141, 87},  // KEYCODE_F11 -> KEY_F11
-        {142, 88},  // KEYCODE_F12 -> KEY_F12
+        { 131, 59 }, // KEYCODE_F1 -> KEY_F1
+        { 132, 60 }, // KEYCODE_F2 -> KEY_F2
+        { 133, 61 }, // KEYCODE_F3 -> KEY_F3
+        { 134, 62 }, // KEYCODE_F4 -> KEY_F4
+        { 135, 63 }, // KEYCODE_F5 -> KEY_F5
+        { 136, 64 }, // KEYCODE_F6 -> KEY_F6
+        { 137, 65 }, // KEYCODE_F7 -> KEY_F7
+        { 138, 66 }, // KEYCODE_F8 -> KEY_F8
+        { 139, 67 }, // KEYCODE_F9 -> KEY_F9
+        { 140, 68 }, // KEYCODE_F10 -> KEY_F10
+        { 141, 87 }, // KEYCODE_F11 -> KEY_F11
+        { 142, 88 }, // KEYCODE_F12 -> KEY_F12
 
         // Navigation
-        {19, 103},  // KEYCODE_DPAD_UP -> KEY_UP
-        {20, 108},  // KEYCODE_DPAD_DOWN -> KEY_DOWN
-        {21, 105},  // KEYCODE_DPAD_LEFT -> KEY_LEFT
-        {22, 106},  // KEYCODE_DPAD_RIGHT -> KEY_RIGHT
-        {23, 28},   // KEYCODE_DPAD_CENTER -> KEY_ENTER
+        { 19, 103 }, // KEYCODE_DPAD_UP -> KEY_UP
+        { 20, 108 }, // KEYCODE_DPAD_DOWN -> KEY_DOWN
+        { 21, 105 }, // KEYCODE_DPAD_LEFT -> KEY_LEFT
+        { 22, 106 }, // KEYCODE_DPAD_RIGHT -> KEY_RIGHT
+        { 23, 28 },  // KEYCODE_DPAD_CENTER -> KEY_ENTER
 
         // Space, Enter, Backspace, Tab, Escape
-        {62, 57},   // KEYCODE_SPACE -> KEY_SPACE
-        {66, 28},   // KEYCODE_ENTER -> KEY_ENTER
-        {67, 14},   // KEYCODE_DEL -> KEY_BACKSPACE
-        {61, 15},   // KEYCODE_TAB -> KEY_TAB
-        {111, 1},   // KEYCODE_ESCAPE -> KEY_ESC
+        { 62, 57 }, // KEYCODE_SPACE -> KEY_SPACE
+        { 66, 28 }, // KEYCODE_ENTER -> KEY_ENTER
+        { 67, 14 }, // KEYCODE_DEL -> KEY_BACKSPACE
+        { 61, 15 }, // KEYCODE_TAB -> KEY_TAB
+        { 111, 1 }, // KEYCODE_ESCAPE -> KEY_ESC
 
         // Shift, Ctrl, Alt, CapsLock, Meta
-        {59, 42},   // KEYCODE_SHIFT_LEFT -> KEY_LEFTSHIFT
-        {60, 54},   // KEYCODE_SHIFT_RIGHT -> KEY_RIGHTSHIFT
-        {113, 29},  // KEYCODE_CTRL_LEFT -> KEY_LEFTCTRL
-        {114, 97},  // KEYCODE_CTRL_RIGHT -> KEY_RIGHTCTRL
-        {57, 56},   // KEYCODE_ALT_LEFT -> KEY_LEFTALT
-        {58, 100},  // KEYCODE_ALT_RIGHT -> KEY_RIGHTALT
-        {115, 58},  // KEYCODE_CAPS_LOCK -> KEY_CAPSLOCK
-        {117, 125}, // KEYCODE_META_LEFT -> KEY_LEFTMETA
-        {118, 126}, // KEYCODE_META_RIGHT -> KEY_RIGHTMETA
+        { 59, 42 },   // KEYCODE_SHIFT_LEFT -> KEY_LEFTSHIFT
+        { 60, 54 },   // KEYCODE_SHIFT_RIGHT -> KEY_RIGHTSHIFT
+        { 113, 29 },  // KEYCODE_CTRL_LEFT -> KEY_LEFTCTRL
+        { 114, 97 },  // KEYCODE_CTRL_RIGHT -> KEY_RIGHTCTRL
+        { 57, 56 },   // KEYCODE_ALT_LEFT -> KEY_LEFTALT
+        { 58, 100 },  // KEYCODE_ALT_RIGHT -> KEY_RIGHTALT
+        { 115, 58 },  // KEYCODE_CAPS_LOCK -> KEY_CAPSLOCK
+        { 117, 125 }, // KEYCODE_META_LEFT -> KEY_LEFTMETA
+        { 118, 126 }, // KEYCODE_META_RIGHT -> KEY_RIGHTMETA
 
         // Symbols
-        {69, 15},   // KEYCODE_TAB -> KEY_TAB
-        {68, 15},   // KEYCODE_TAB (duplicate, for compatibility)
-        {76, 127},  // KEYCODE_MOVE_HOME -> KEY_HOME
-        {122, 102}, // KEYCODE_MOVE_HOME -> KEY_HOME
-        {123, 107}, // KEYCODE_MOVE_END -> KEY_END
-        {124, 104}, // KEYCODE_PAGE_UP -> KEY_PAGEUP
-        {92, 109},  // KEYCODE_PAGE_DOWN -> KEY_PAGEDOWN
-        {112, 111}, // KEYCODE_FORWARD_DEL -> KEY_DELETE
+        { 69, 15 },   // KEYCODE_TAB -> KEY_TAB
+        { 68, 15 },   // KEYCODE_TAB (duplicate, for compatibility)
+        { 76, 127 },  // KEYCODE_MOVE_HOME -> KEY_HOME
+        { 122, 102 }, // KEYCODE_MOVE_HOME -> KEY_HOME
+        { 123, 107 }, // KEYCODE_MOVE_END -> KEY_END
+        { 124, 104 }, // KEYCODE_PAGE_UP -> KEY_PAGEUP
+        { 92, 109 },  // KEYCODE_PAGE_DOWN -> KEY_PAGEDOWN
+        { 112, 111 }, // KEYCODE_FORWARD_DEL -> KEY_DELETE
 
         // Misc
-        {4, 158},   // KEYCODE_BACK -> KEY_BACK
-        {3, 102},   // KEYCODE_HOME -> KEY_HOME
-        {82, 139},  // KEYCODE_MENU -> KEY_MENU
-        {84, 114},  // KEYCODE_SEARCH -> KEY_SEARCH
-        {85, 164},  // KEYCODE_MEDIA_PLAY_PAUSE -> KEY_PLAYPAUSE
-        {86, 128},  // KEYCODE_MEDIA_STOP -> KEY_STOPCD
-        {87, 163},  // KEYCODE_MEDIA_NEXT -> KEY_NEXTSONG
-        {88, 165},  // KEYCODE_MEDIA_PREVIOUS -> KEY_PREVIOUSSONG
-        {89, 168},  // KEYCODE_MEDIA_REWIND -> KEY_REWIND
-        {90, 208},  // KEYCODE_MEDIA_FAST_FORWARD -> KEY_FASTFORWARD
-        {24, 115},  // KEYCODE_VOLUME_UP -> KEY_VOLUMEUP
-        {25, 114},  // KEYCODE_VOLUME_DOWN -> KEY_VOLUMEDOWN
-        {164, 113}, // KEYCODE_VOLUME_MUTE -> KEY_MUTE
+        { 4, 158 },   // KEYCODE_BACK -> KEY_BACK
+        { 3, 102 },   // KEYCODE_HOME -> KEY_HOME
+        { 82, 139 },  // KEYCODE_MENU -> KEY_MENU
+        { 84, 114 },  // KEYCODE_SEARCH -> KEY_SEARCH
+        { 85, 164 },  // KEYCODE_MEDIA_PLAY_PAUSE -> KEY_PLAYPAUSE
+        { 86, 128 },  // KEYCODE_MEDIA_STOP -> KEY_STOPCD
+        { 87, 163 },  // KEYCODE_MEDIA_NEXT -> KEY_NEXTSONG
+        { 88, 165 },  // KEYCODE_MEDIA_PREVIOUS -> KEY_PREVIOUSSONG
+        { 89, 168 },  // KEYCODE_MEDIA_REWIND -> KEY_REWIND
+        { 90, 208 },  // KEYCODE_MEDIA_FAST_FORWARD -> KEY_FASTFORWARD
+        { 24, 115 },  // KEYCODE_VOLUME_UP -> KEY_VOLUMEUP
+        { 25, 114 },  // KEYCODE_VOLUME_DOWN -> KEY_VOLUMEDOWN
+        { 164, 113 }, // KEYCODE_VOLUME_MUTE -> KEY_MUTE
     };
 
     auto it = kMap.find(key);
