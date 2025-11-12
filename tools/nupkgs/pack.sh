bfae416337795e30d927bd5061730951828997a8
moomiji <35213527+moomiji@users.noreply.github.com>
2025-11-12 19:39:35 +0800

    chore: better .targets template

--- tools/nupkgs/pack.sh
+++ tools/nupkgs/pack.sh
@@ -5,20 +5,20 @@ IFS=$'\n\t'
 version="$1"
 
 curl -fsSL -O https://raw.githubusercontent.com/MaaAssistantArknights/design/main/logo/maa-logo_128x128.png
-for NUSPEC in Maa.Framework.Runtime*.nuspec; do
+for nuspec in Maa.Framework.Runtime*.nuspec; do
     # extract RID from filename:
-    #     Maa.Framework.Runtime.<rid>.nuspec -> <rid>
+    #     Maa.Framework.Runtime.<rid>.nuspec -> <rid> Maa.Framework.Runtime.<rid>
     #     Maa.Framework.Runtimes.nuspec      -> Maa.Framework.Runtimes
-    rid="${NUSPEC#Maa.Framework.Runtime.}"
-    rid="${rid%.nuspec}"
-    if [ "${rid}" != "Maa.Framework.Runtimes" ]; then
-        sed "s/__RID__/${rid}/g" "buildTransitive/template.targets" > "buildTransitive/Maa.Framework.Runtime.${rid}.targets"
+    pid="${nuspec%.nuspec}"
+    rid="${pid#Maa.Framework.Runtime.}"
+    if [ "${pid}" != "Maa.Framework.Runtimes" ]; then
+        sed "s/__PID__/${pid}/g; s/__RID__/${rid}/g" "buildTransitive/template.targets" > "buildTransitive/${pid}.targets"
     fi
 
     # Prepend Head.nuspec and append Tail.nuspec
-    cat Head.nuspec "${NUSPEC}" Tail.nuspec > temp && mv temp "${NUSPEC}"
+    cat Head.nuspec "${nuspec}" Tail.nuspec > temp && mv temp "${nuspec}"
 
-    nuget pack "${NUSPEC}" \
+    nuget pack "${nuspec}" \
         -Verbosity detailed \
         -Properties "version=${version//-post/};year=$(date +%Y);branch=$(git rev-parse --abbrev-ref HEAD);commit=$(git rev-parse HEAD);root_path=..\.."
 done
