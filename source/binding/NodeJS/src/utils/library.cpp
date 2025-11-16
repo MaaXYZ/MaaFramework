6cd3a5c79ca7daf41663b6a3213193918ac3dceb
nekosu <liao.hy@outlook.com>
2025-11-16 22:53:57 +0800

    fix(js): windows agent path

--- source/binding/NodeJS/src/utils/library.cpp
+++ source/binding/NodeJS/src/utils/library.cpp
@@ -3,7 +3,44 @@
 #if defined(_WIN32)
 
 #include <Windows.h>
+#include <fileapi.h>
 #include <psapi.h>
+
+// \Device\HarddiskVolume3\xxx -> C:\xxx
+static std::wstring NtPathToDosPath(std::wstring nt_path)
+{
+    WCHAR drive_buffers[512];
+
+    DWORD len = GetLogicalDriveStringsW(512, drive_buffers);
+    if (!len) {
+        return L"";
+    }
+
+    std::vector<std::wstring> drives;
+    WCHAR* p = drive_buffers;
+    while (*p) {
+        drives.emplace_back(p);
+        p += wcslen(p) + 1;
+    }
+
+    WCHAR device_paths[512];
+    for (const auto& drive : drives) {
+        std::wstring drive_name = drive.substr(0, 2);
+
+        DWORD ret = QueryDosDeviceW(drive_name.c_str(), device_paths, 512);
+        if (!ret) {
+            continue;
+        }
+
+        std::wstring dev { device_paths };
+        if (nt_path.rfind(dev, 0) == 0) {
+            nt_path.replace(0, dev.size(), drive_name);
+            return nt_path;
+        }
+    }
+
+    return L"";
+}
 
 std::filesystem::path get_library_path(void* addr)
 {
@@ -14,7 +51,7 @@ std::filesystem::path get_library_path(void* addr)
         return {};
     }
 
-    return { path_buf };
+    return { NtPathToDosPath(path_buf) };
 }
 
 #else
