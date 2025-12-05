0bbbb0fbc7edffe1d9b3b26f0374166e56a8f740
uye <99072975+ABA2396@users.noreply.github.com>
2025-12-06 03:36:03 +0800

    fix: controller auto detect (#925)
    Co-authored-by: MistEO <mistereo@hotmail.com>

--- source/MaaToolkit/AdbDevice/AdbDeviceFinder.h
+++ source/MaaToolkit/AdbDevice/AdbDeviceFinder.h
@@ -30,10 +30,10 @@ public:
     struct Emulator
     {
         std::string name;
-        ProcessInfo process;
-        EmulatorConstantData const_data;
+        std::filesystem::path process_path;
+        std::filesystem::path adb_path;
 
-        MEO_TOJSON(name, process, const_data);
+        MEO_TOJSON(name, adb_path);
     };
 
 public:
@@ -44,22 +44,18 @@ public:
     std::vector<AdbDevice> find_specified(const std::filesystem::path& adb_path) const;
 
 protected:
-    virtual std::vector<std::string> find_adb_serials(const std::filesystem::path& adb_path, const Emulator& emulator) const;
-    virtual void request_device_config(const Emulator& emulator, AdbDevice& device) const;
+    virtual std::vector<AdbDevice> find_by_emulator_tool(const Emulator& emulator) const;
 
 protected:
     void set_emulator_const_data(std::unordered_map<std::string, EmulatorConstantData> data);
 
-    std::vector<Emulator> find_emulators() const;
-    std::filesystem::path get_adb_path(const EmulatorConstantData& emulator, os_pid pid) const;
-
     std::vector<std::string> find_serials_by_adb_command(const std::filesystem::path& adb_path) const;
-    bool request_adb_connect(const std::filesystem::path& adb_path, const std::string& serial) const;
-    std::vector<std::string>
-        check_available_adb_serials(const std::filesystem::path& adb_path, const std::vector<std::string>& serials) const;
+    std::optional<AdbDevice> try_device(const std::filesystem::path& adb_path, const std::string& serial) const;
+
+    std::vector<Emulator> find_emulators() const;
+    std::filesystem::path get_emulator_adb_path(const EmulatorConstantData& emulator, os_pid pid) const;
 
 private:
-    std::optional<std::vector<AdbDevice>> devices_;
     std::unordered_map<std::string, EmulatorConstantData> const_data_;
 };
 
