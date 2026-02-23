32492185235d51d3193b573e9988f2aeede8d02d
YJBeetle <YJBeetle@gmail.com>
2026-02-24 02:08:44 +0800

    feat: 添加键盘和鼠标事件处理功能，重构事件处理逻辑，打印原始键对应字符，

--- test/macos_test/MacOSTestGUI.mm
+++ test/macos_test/MacOSTestGUI.mm
@@ -5,10 +5,22 @@
 #include "./MacOSTestGUI.h"
 
 // 自定义视图类，用于处理键盘和鼠标事件
-@interface EventHandlingView : NSView
+@interface EventHandlingView : NSView {
+    MacOSTestGUI* _guiInstance;
+}
+- (instancetype)initWithFrame:(NSRect)frameRect gui:(MacOSTestGUI*)gui;
 @end
 
 @implementation EventHandlingView
+
+- (instancetype)initWithFrame:(NSRect)frameRect gui:(MacOSTestGUI*)gui
+{
+    self = [super initWithFrame:frameRect];
+    if (self) {
+        _guiInstance = gui;
+    }
+    return self;
+}
 
 - (BOOL)acceptsFirstResponder
 {
@@ -17,52 +29,85 @@
 
 - (void)keyDown:(NSEvent*)event
 {
-    unsigned short keyCode = [event keyCode];
-    NSEventModifierFlags modifiers = [event modifierFlags];
-    NSString* characters = [event characters];
-    NSString* charactersIgnoringModifiers = [event charactersIgnoringModifiers];
-
-    std::cout << std::endl;
-
-    // 打印按键修饰符
-    if (modifiers & NSEventModifierFlagCommand) {
-        std::cout << "⌘ ";
-    }
-    if (modifiers & NSEventModifierFlagShift) {
-        std::cout << "⇧ ";
-    }
-    if (modifiers & NSEventModifierFlagControl) {
-        std::cout << "⌃ ";
-    }
-    if (modifiers & NSEventModifierFlagOption) {
-        std::cout << "⌥ ";
-    }
-    if (modifiers & NSEventModifierFlagCapsLock) {
-        std::cout << "⇪ ";
-    }
-    std::cout << "KeyCode: " << keyCode << std::endl;
-
-    if (characters && [characters length] > 0) {
-        std::cout << "Characters: '" << [characters UTF8String] << "'" << std::endl;
-    }
+    _guiInstance->handleKeyEvent(
+        MacOSTestGUI::KeyDown, [event keyCode], [event modifierFlags], [[event characters] UTF8String],
+        [[event charactersIgnoringModifiers] UTF8String]);
+}
+- (void)keyUp:(NSEvent*)event
+{
+    _guiInstance->handleKeyEvent(
+        MacOSTestGUI::KeyUp, [event keyCode], [event modifierFlags], [[event characters] UTF8String],
+        [[event charactersIgnoringModifiers] UTF8String]);
 }
 
 - (void)mouseDown:(NSEvent*)event
 {
     NSPoint location = [event locationInWindow];
-    std::cout << "鼠标点击坐标: (" << location.x << ", " << location.y << ")" << std::endl;
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseDown, MacOSTestGUI::Left, location.x, location.y, (unsigned long long)[event modifierFlags]);
 }
-
 - (void)mouseDragged:(NSEvent*)event
 {
     NSPoint location = [event locationInWindow];
-    std::cout << "鼠标拖动坐标: (" << location.x << ", " << location.y << ")" << std::endl;
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseMove, MacOSTestGUI::Left, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)mouseUp:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseUp, MacOSTestGUI::Left, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)rightMouseDown:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseDown, MacOSTestGUI::Right, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)rightMouseDragged:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseMove, MacOSTestGUI::Right, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)rightMouseUp:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseUp, MacOSTestGUI::Right, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)otherMouseDown:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseDown, MacOSTestGUI::Other, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)otherMouseDragged:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseMove, MacOSTestGUI::Other, location.x, location.y, (unsigned long long)[event modifierFlags]);
+}
+- (void)otherMouseUp:(NSEvent*)event
+{
+    NSPoint location = [event locationInWindow];
+    _guiInstance->handleMouseEvent(
+        MacOSTestGUI::MouseUp, MacOSTestGUI::Other, location.x, location.y, (unsigned long long)[event modifierFlags]);
 }
 
 @end
 
 MacOSTestGUI::MacOSTestGUI(const std::string& windowTitle)
 {
+    // 获取当前键盘布局
+    keyboardLayout = TISCopyCurrentKeyboardLayoutInputSource();
+    if (keyboardLayout) {
+        CFDataRef layoutData = (CFDataRef)TISGetInputSourceProperty(keyboardLayout, kTISPropertyUnicodeKeyLayoutData);
+        if (layoutData) {
+            keyboardLayoutData = (const UCKeyboardLayout*)CFDataGetBytePtr(layoutData);
+        }
+    }
+
     NSApplication* app = [NSApplication sharedApplication];
 
     // 设置应用为普通前台应用，否则无法接收键盘事件
@@ -82,7 +127,7 @@ MacOSTestGUI::MacOSTestGUI(const std::string& windowTitle)
     [window setLevel:NSFloatingWindowLevel];
 
     // 创建自定义容器视图（支持事件处理）
-    EventHandlingView* containerView = [[EventHandlingView alloc] initWithFrame:NSMakeRect(0, 0, 400, 400)];
+    EventHandlingView* containerView = [[EventHandlingView alloc] initWithFrame:NSMakeRect(0, 0, 400, 400) gui:this];
 
     // 创建文本标签（上面）
     NSTextField* textField = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 320, 400, 60)];
@@ -112,10 +157,102 @@ MacOSTestGUI::MacOSTestGUI(const std::string& windowTitle)
 
 MacOSTestGUI::~MacOSTestGUI()
 {
+    // 释放键盘布局资源
+    if (keyboardLayout) {
+        CFRelease(keyboardLayout);
+        keyboardLayout = nullptr;
+        keyboardLayoutData = nullptr;
+    }
 }
 
 void MacOSTestGUI::run()
 {
     NSApplication* app = [NSApplication sharedApplication];
     [app run];
+}
+
+void MacOSTestGUI::handleKeyEvent(
+    KbdEventType type, unsigned short keyCode, uint64_t modifiers, std::string characters, std::string charactersIgnoringModifiers)
+{
+    std::cout << std::endl;
+
+    // 打印按键
+    std::cout << "Key: ";
+    if (modifiers & NSEventModifierFlagCommand) {
+        std::cout << "⌘ ";
+    }
+    if (modifiers & NSEventModifierFlagShift) {
+        std::cout << "⇧ ";
+    }
+    if (modifiers & NSEventModifierFlagControl) {
+        std::cout << "⌃ ";
+    }
+    if (modifiers & NSEventModifierFlagOption) {
+        std::cout << "⌥ ";
+    }
+    if (modifiers & NSEventModifierFlagCapsLock) {
+        std::cout << "⇪ ";
+    }
+    // 使用缓存的键盘布局获取按键名称
+    std::cout << keyCodeToString(keyCode) << std::endl;
+
+    std::cout << "KeyCode: " << keyCode << std::endl;
+
+    std::cout << "Characters: '" << characters << "'" << std::endl;
+}
+
+void MacOSTestGUI::handleMouseEvent(MouseEventType type, MouseButton button, double x, double y, unsigned long long modifiers)
+{
+    std::cout << "鼠标";
+    switch (type) {
+    case MouseDown:
+        std::cout << "按下";
+        break;
+    case MouseUp:
+        std::cout << "释放";
+        break;
+    case MouseMove:
+        std::cout << "移动";
+        break;
+    }
+
+    std::cout << " - ";
+    switch (button) {
+    case Left:
+        std::cout << "左键";
+        break;
+    case Right:
+        std::cout << "右键";
+        break;
+    case Other:
+        std::cout << "其他";
+        break;
+    }
+
+    std::cout << " 坐标: (" << x << ", " << y << ")" << std::endl;
+}
+
+std::string MacOSTestGUI::keyCodeToString(unsigned short keyCode)
+{
+    if (!keyboardLayoutData) {
+        return "";
+    }
+
+    // 尝试获取按键名称
+    UniChar unicodeString[4];
+    UniCharCount actualStringLength = 0;
+    UInt32 deadKeyState = 0;
+
+    // 使用UCKeyTranslate获取按键对应的字符
+    OSStatus status = UCKeyTranslate(
+        keyboardLayoutData, keyCode, kUCKeyActionDisplay,
+        0, // 无修饰符
+        LMGetKbdType(), kUCKeyTranslateNoDeadKeysBit, &deadKeyState, sizeof(unicodeString) / sizeof(unicodeString[0]), &actualStringLength,
+        unicodeString);
+
+    if (status == noErr && actualStringLength > 0) {
+        return std::string([NSString stringWithCharacters:unicodeString length:actualStringLength].UTF8String);
+    }
+
+    return "";
 }
