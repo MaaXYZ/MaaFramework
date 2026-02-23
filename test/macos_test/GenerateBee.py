1f74fca68ebc9b64b4193daa7cc16d5592ae1b57
YJBeetle <YJBeetle@gmail.com>
2026-02-24 06:09:38 +0800

    feat: 使用Pilmoji库绘制彩色emoji

--- test/macos_test/GenerateBee.py
+++ test/macos_test/GenerateBee.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageFont
+from pilmoji import Pilmoji
+from pilmoji.source import AppleEmojiSource
 
 def generate_bee_template():
-    img = Image.new('RGB', (60, 60), (255, 255, 255))
-    draw = ImageDraw.Draw(img)
+    img = Image.new('RGB', (60, 60), (46, 41, 45))
 
     font = ImageFont.truetype('/System/Library/Fonts/Apple Color Emoji.ttc', 40)
 
-    draw.text((10, 10), '🐝', fill=(0, 0, 0), font=font)
+    with Pilmoji(img, source=AppleEmojiSource) as pilmoji:
+        pilmoji.text((10, -30), '🐝', font=font)
 
     img.save('Bee.png')
 
