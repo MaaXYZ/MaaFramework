f5384c91cb8f14a0195630bc186021c8ef622e39
nekosu <liao.hy@outlook.com>
2026-03-09 21:51:29 +0800

    fix(js): update types

--- source/binding/NodeJS/src/apis/global.d.ts
+++ source/binding/NodeJS/src/apis/global.d.ts
@@ -1,14 +1,14 @@
 declare global {
     namespace maa {
+        type StdoutLevel = 'Off' | 'Fatal' | 'Error' | 'Warn' | 'Info' | 'Debug' | 'Trace' | 'All'
+
         const Global: {
             get version_from_macro(): string
             get version(): string
             set log_dir(value: string)
             set save_draw(value: boolean)
             set save_on_error(value: boolean)
-            set stdout_level(
-                value: 'Off' | 'Fatal' | 'Error' | 'Warn' | 'Info' | 'Debug' | 'Trace' | 'All',
-            )
+            set stdout_level(value: StdoutLevel)
             set debug_mode(value: boolean)
             set draw_quality(value: number)
             set reco_image_cache_limit(value: number)
