1b766ae47db3094e05eb3c4d8c8410d2fec2dcde
overflow65537 <err.overflow@gmail.com>
2026-07-12 19:17:51 +0800

    fix(AdbDeviceWin32Finder): 修正 MuMuPlayer v5+ 的名称和路径注释

--- source/MaaToolkit/AdbDevice/AdbDeviceWin32Finder.cpp
+++ source/MaaToolkit/AdbDevice/AdbDeviceWin32Finder.cpp
@@ -160,12 +160,12 @@ std::vector<AdbDevice> AdbDeviceWin32Finder::find_mumu_devices(const Emulator& e
     }
 
     std::filesystem::path dir;
-    if (emulator.name == "MuMuPlayer12 v5") {
-        // MuMuPlayer12 v5: C:\Program Files\Netease\MuMuPlayer-12.0\nx_device\12.0\shell\MuMuNxDevice.exe
+    if (emulator.name == "MuMuPlayer v5+") {
+        // MuMuPlayer v5+: C:\Program Files\Netease\MuMuPlayer-12.0\nx_device\12.0\shell\MuMuNxDevice.exe
         dir = emulator.process_path.parent_path().parent_path().parent_path().parent_path();
     }
     else {
-        // MuMuPlayer12: C:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuPlayer.exe
+        // MuMuPlayer v4: C:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuPlayer.exe
         dir = emulator.process_path.parent_path().parent_path();
     }
 
