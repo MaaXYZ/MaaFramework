b03ae6a1bcacca84c703581da0883ecb27d3028b
litwak913 <susie@goldenglow.dev>
2026-08-08 13:29:38 +0800

    fix(linux): return nullopt

--- source/MaaLinuxControlUnit/Screencap/NoneScreencap.h
+++ source/MaaLinuxControlUnit/Screencap/NoneScreencap.h
@@ -12,7 +12,7 @@ public:
 
     virtual ~NoneScreencap() override = default;
 
-    std::optional<cv::Mat> screencap() override { return cv::Mat(); }
+    std::optional<cv::Mat> screencap() override { return std::nullopt; }
 };
 
 MAA_CTRL_UNIT_NS_END
