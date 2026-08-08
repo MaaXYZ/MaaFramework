04af2c42ca00c42a8117a963d8439efab8022acb
litwak913 <susie@goldenglow.dev>
2026-08-08 12:31:16 +0800

    test(linux): add pipewire screencap test

---
+++ test/linux_test/pw_screencap_test.cpp
@@ -0,0 +1,55 @@
+#include "MaaFramework/Instance/MaaController.h"
+#include "MaaFramework/Utility/MaaBuffer.h"
+
+#include <iostream>
+
+#include "MaaToolkit/MaaToolkitAPI.h"
+
+//
+// Created by free on 8/8/26.
+//
+int main()
+{
+    const auto helper = MaaToolkitPortalHelperCreate();
+    std::string restore_token;
+    std::cout << "Input Restore Token, press Enter to skip: ";
+    getline(std::cin, restore_token);
+    if (!restore_token.empty()) {
+        MaaToolkitPortalHelperSetRestoreToken(helper, restore_token.c_str());
+    }
+    MaaToolkitPortalHelperSetPersist(helper, true);
+    if (!MaaToolkitPortalHelperOpenStream(helper)) {
+        std::cerr << "Failed to open PipeWire Stream" << std::endl;
+        return -1;
+    }
+    auto pw_socket_fd = MaaToolkitPortalHelperGetPipeWireFD(helper);
+    auto pw_node_id = MaaToolkitPortalHelperGetPipeWireNodeID(helper);
+    auto restore = MaaToolkitPortalHelperGetRestoreToken(helper);
+    std::cout << "Socket FD: " << pw_socket_fd << std::endl;
+    std::cout << "Node ID: " << pw_node_id << std::endl;
+    if (restore != nullptr) {
+        std::cout << "Restore Token: " << restore << std::endl;
+    }
+    auto ctrl = MaaLinuxControllerCreate(
+        std::format(
+            R"({{"screencap_method":4,"input_method":0, "pw_socket_fd":{}, "pw_node_id":{}, "pw_screen_width":1920, "pw_screen_height":1080}})",
+            pw_socket_fd,
+            pw_node_id)
+            .c_str());
+    auto destroy = [&]() {
+        MaaControllerDestroy(ctrl);
+        MaaToolkitPortalHelperDestroy(helper);
+    };
+    auto ctrl_id = MaaControllerPostConnection(ctrl);
+    MaaControllerWait(ctrl, ctrl_id);
+    ctrl_id = MaaControllerPostScreencap(ctrl);
+    MaaControllerWait(ctrl, ctrl_id);
+    auto imgbuf = MaaImageBufferCreate(); // Add breakpoint at here to view image
+    auto img = MaaControllerCachedImage(ctrl, imgbuf);
+    if (!img) {
+        destroy();
+        return -1;
+    }
+    destroy();
+    return 0;
+}
